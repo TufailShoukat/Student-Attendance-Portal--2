1fb00dc164304c7e1d0ccead6acaed51dae355c1
Tufail Shoukat <tufailshoukat72@gmail.com>
2024-05-31 16:37:43 +0500

    js.file created

---
+++ app.js
@@ -0,0 +1,394 @@
+// Define functions to handle button clicks for each student
+function PresentButton() {
+    var presentCount = parseInt(document.getElementById("PRESENT").textContent);
+    document.getElementById("PRESENT").textContent = presentCount + 1;
+    
+    
+}
+
+function absentButton() {
+    var absentCount = parseInt(document.getElementById("ABSENT").textContent);
+    document.getElementById("ABSENT").textContent = absentCount + 1;
+
+}
+
+function leaveButton() {
+    var leaveCount = parseInt(document.getElementById("LEAVE").textContent);
+    document.getElementById("LEAVE").textContent = leaveCount + 1;
+    
+}
+
+//////
+function PresentButton2() {
+    var presentCount = parseInt(document.getElementById("PRESENT2").textContent);
+    document.getElementById("PRESENT2").textContent = presentCount + 1;
+    
+}
+
+function absentButton2() {
+    var absentCount = parseInt(document.getElementById("ABSENT2").textContent);
+    document.getElementById("ABSENT2").textContent = absentCount + 1;
+    
+}
+
+
+function leaveButton2() {
+    var leaveCount = parseInt(document.getElementById("LEAVE2").textContent);
+    document.getElementById("LEAVE2").textContent = leaveCount + 1;
+    
+}
+
+//
+
+//////
+function PresentButton3() {
+    var presentCount = parseInt(document.getElementById("PRESENT3").textContent);
+    document.getElementById("PRESENT3").textContent = presentCount + 1;
+}
+
+function absentButton3() {
+    var absentCount = parseInt(document.getElementById("ABSENT3").textContent);
+    document.getElementById("ABSENT3").textContent = absentCount + 1;
+
+    
+}
+
+
+function leaveButton3() {
+    var leaveCount = parseInt(document.getElementById("LEAVE3").textContent);
+    document.getElementById("LEAVE3").textContent = leaveCount + 1;
+    
+}
+function PresentButton4() {
+    var presentCount = parseInt(document.getElementById("PRESENT4").textContent);
+    document.getElementById("PRESENT4").textContent = presentCount + 1;
+  
+  
+}
+
+function absentButton4() {
+    var absentCount = parseInt(document.getElementById("ABSENT4").textContent);
+    document.getElementById("ABSENT4").textContent = absentCount + 1;
+    
+    
+}
+
+function leaveButton4() {
+    var leaveCount = parseInt(document.getElementById("LEAVE4").textContent);
+    document.getElementById("LEAVE4").textContent = leaveCount + 1;
+    
+}
+
+
+function PresentButton5() {
+    var presentCount = parseInt(document.getElementById("PRESENT5").textContent);
+    document.getElementById("PRESENT5").textContent = presentCount + 1;
+
+}
+
+function absentButton5() {
+    var absentCount = parseInt(document.getElementById("ABSENT5").textContent);
+    document.getElementById("ABSENT5").textContent = absentCount + 1;
+    
+}
+
+function leaveButton5() {
+    var leaveCount = parseInt(document.getElementById("LEAVE5").textContent);
+    document.getElementById("LEAVE5").textContent = leaveCount + 1;
+    
+}//
+function PresentButton6() {
+    var presentCount = parseInt(document.getElementById("PRESENT6").textContent);
+    document.getElementById("PRESENT6").textContent = presentCount + 1;
+    
+    
+}
+
+function absentButton6() {
+    var absentCount = parseInt(document.getElementById("ABSENT6").textContent);
+    document.getElementById("ABSENT6").textContent = absentCount + 1;
+  
+  
+
+}
+
+function leaveButton6() {
+    var leaveCount = parseInt(document.getElementById("LEAVE6").textContent);
+    document.getElementById("LEAVE6").textContent = leaveCount + 1;
+
+}
+////
+function PresentButton7() {
+    var presentCount = parseInt(document.getElementById("PRESENT7").textContent);
+    document.getElementById("PRESENT7").textContent = presentCount + 1;
+
+}
+
+function absentButton7() {
+    var absentCount = parseInt(document.getElementById("ABSENT7").textContent);
+    document.getElementById("ABSENT7").textContent = absentCount + 1;
+}
+
+function leaveButton7() {
+    var leaveCount = parseInt(document.getElementById("LEAVE7").textContent);
+    document.getElementById("LEAVE7").textContent = leaveCount + 1;
+}
+
+function PresentButton8() {
+    var presentCount = parseInt(document.getElementById("PRESENT8").textContent);
+    document.getElementById("PRESENT8").textContent = presentCount + 1;
+}
+
+function absentButton8() {
+    var absentCount = parseInt(document.getElementById("ABSENT8").textContent);
+    document.getElementById("ABSENT8").textContent = absentCount + 1;
+}
+
+function leaveButton8() {
+    var leaveCount = parseInt(document.getElementById("LEAVE8").textContent);
+    document.getElementById("LEAVE8").textContent = leaveCount + 1;
+}
+
+
+
+
+function PresentButton9() {
+    var presentCount = parseInt(document.getElementById("PRESENT9").textContent);
+    document.getElementById("PRESENT9").textContent = presentCount + 1;
+}
+
+function absentButton9() {
+    var absentCount = parseInt(document.getElementById("ABSENT9").textContent);
+    document.getElementById("ABSENT9").textContent = absentCount + 1;
+}
+
+function leaveButton9() {
+    var leaveCount = parseInt(document.getElementById("LEAVE9").textContent);
+    document.getElementById("LEAVE9").textContent = leaveCount + 1;
+}
+///
+function PresentButton10() {
+    var presentCount = parseInt(document.getElementById("PRESENT10").textContent);
+    document.getElementById("PRESENT10").textContent = presentCount + 1;
+}
+
+function absentButton10() {
+    var absentCount = parseInt(document.getElementById("ABSENT10").textContent);
+    document.getElementById("ABSENT10").textContent = absentCount + 1;
+}
+
+function leaveButton10() {
+    var leaveCount = parseInt(document.getElementById("LEAVE10").textContent);
+    document.getElementById("LEAVE10").textContent = leaveCount + 1;
+}
+
+function PresentButton11() {
+    var presentCount = parseInt(document.getElementById("PRESENT11").textContent);
+    document.getElementById("PRESENT11").textContent = presentCount + 1;
+}
+
+function absentButton11() {
+    var absentCount = parseInt(document.getElementById("ABSENT11").textContent);
+    document.getElementById("ABSENT11").textContent = absentCount + 1;
+}
+
+function leaveButton11() {
+    var leaveCount = parseInt(document.getElementById("LEAVE11").textContent);
+    document.getElementById("LEAVE11").textContent = leaveCount + 1;
+}
+////
+function PresentButton12() {
+    var presentCount = parseInt(document.getElementById("PRESENT12").textContent);
+    document.getElementById("PRESENT12").textContent = presentCount + 1;
+}
+
+function absentButton12() {
+    var absentCount = parseInt(document.getElementById("ABSENT12").textContent);
+    document.getElementById("ABSENT12").textContent = absentCount + 1;
+}
+
+function leaveButton12() {
+    var leaveCount = parseInt(document.getElementById("LEAVE12").textContent);
+    document.getElementById("LEAVE12").textContent = leaveCount + 1;
+}
+
+
+function PresentButton13() {
+    var presentCount = parseInt(document.getElementById("PRESENT13").textContent);
+    document.getElementById("PRESENT13").textContent = presentCount + 1;
+}
+
+function absentButton13() {
+    var absentCount = parseInt(document.getElementById("ABSENT13").textContent);
+    document.getElementById("ABSENT13").textContent = absentCount + 1;
+}
+
+function leaveButton13() {
+    var leaveCount = parseInt(document.getElementById("LEAVE13").textContent);
+    document.getElementById("LEAVE13").textContent = leaveCount + 1;
+}
+
+
+function PresentButton14() {
+    var presentCount = parseInt(document.getElementById("PRESENT14").textContent);
+    document.getElementById("PRESENT14").textContent = presentCount + 1;
+}
+
+function absentButton14() {
+    var absentCount = parseInt(document.getElementById("ABSENT14").textContent);
+    document.getElementById("ABSENT14").textContent = absentCount + 1;
+}
+
+function leaveButton14() {
+    var leaveCount = parseInt(document.getElementById("LEAVE14").textContent);
+    document.getElementById("LEAVE14").textContent = leaveCount + 1;
+}
+
+
+function PresentButton15() {
+    var presentCount = parseInt(document.getElementById("PRESENT15").textContent);
+    document.getElementById("PRESENT15").textContent = presentCount + 1;
+}
+
+function absentButton15() {
+    var absentCount = parseInt(document.getElementById("ABSENT15").textContent);
+    document.getElementById("ABSENT15").textContent = absentCount + 1;
+}
+
+function leaveButton15() {
+    var leaveCount = parseInt(document.getElementById("LEAVE15").textContent);
+    document.getElementById("LEAVE15").textContent = leaveCount + 1;
+}
+///
+function PresentButton16() {
+    var presentCount = parseInt(document.getElementById("PRESENT16").textContent);
+    document.getElementById("PRESENT16").textContent = presentCount + 1;
+}
+
+function absentButton16() {
+    var absentCount = parseInt(document.getElementById("ABSENT16").textContent);
+    document.getElementById("ABSENT16").textContent = absentCount + 1;
+}
+
+function leaveButton16() {
+    var leaveCount = parseInt(document.getElementById("LEAVE16").textContent);
+    document.getElementById("LEAVE16").textContent = leaveCount + 1;
+}
+function PresentButton17() {
+    var presentCount = parseInt(document.getElementById("PRESENT17").textContent);
+    document.getElementById("PRESENT17").textContent = presentCount + 1;
+}
+
+function absentButton17() {
+    var absentCount = parseInt(document.getElementById("ABSENT17").textContent);
+    document.getElementById("ABSENT17").textContent = absentCount + 1;
+}
+
+function leaveButton17() {
+    var leaveCount = parseInt(document.getElementById("LEAVE17").textContent);
+    document.getElementById("LEAVE17").textContent = leaveCount + 1;
+}
+
+
+
+function PresentButton18() {
+    var presentCount = parseInt(document.getElementById("PRESENT18").textContent);
+    document.getElementById("PRESENT18").textContent = presentCount + 1;
+}
+
+function absentButton18() {
+    var absentCount = parseInt(document.getElementById("ABSENT18").textContent);
+    document.getElementById("ABSENT18").textContent = absentCount + 1;
+}
+
+function leaveButton18() {
+    var leaveCount = parseInt(document.getElementById("LEAVE18").textContent);
+    document.getElementById("LEAVE18").textContent = leaveCount + 1;
+}
+
+function PresentButton19() {
+    var presentCount = parseInt(document.getElementById("PRESENT19").textContent);
+    document.getElementById("PRESENT19").textContent = presentCount + 1;
+}
+
+function absentButton19() {
+    var absentCount = parseInt(document.getElementById("ABSENT19").textContent);
+    document.getElementById("ABSENT19").textContent = absentCount + 1;
+}
+
+function leaveButton19() {
+    var leaveCount = parseInt(document.getElementById("LEAVE19").textContent);
+    document.getElementById("LEAVE19").textContent = leaveCount + 1;
+}
+
+function PresentButton19() {
+    var presentCount = parseInt(document.getElementById("PRESENT19").textContent);
+    document.getElementById("PRESENT19").textContent = presentCount + 1;
+}
+
+function absentButton19() {
+    var absentCount = parseInt(document.getElementById("ABSENT19").textContent);
+    document.getElementById("ABSENT19").textContent = absentCount + 1;
+}
+
+function leaveButton19() {
+    var leaveCount = parseInt(document.getElementById("LEAVE19").textContent);
+    document.getElementById("LEAVE19").textContent = leaveCount + 1;
+}
+function PresentButton21() {
+    var presentCount = parseInt(document.getElementById("PRESENT21").textContent);
+    document.getElementById("PRESENT21").textContent = presentCount + 1;
+}
+
+function absentButton21() {
+    var absentCount = parseInt(document.getElementById("ABSENT21").textContent);
+    document.getElementById("ABSENT21").textContent = absentCount + 1;
+}
+
+function leaveButton21() {
+    var leaveCount = parseInt(document.getElementById("LEAVE21").textContent);
+    document.getElementById("LEAVE21").textContent = leaveCount + 1;
+}
+
+function PresentButton22() {
+    var presentCount = parseInt(document.getElementById("PRESENT22").textContent);
+    document.getElementById("PRESENT22").textContent = presentCount + 1;
+}
+
+function absentButton22() {
+    var absentCount = parseInt(document.getElementById("ABSENT22").textContent);
+    document.getElementById("ABSENT22").textContent = absentCount + 1;
+}
+
+function leaveButton22() {
+    var leaveCount = parseInt(document.getElementById("LEAVE22").textContent);
+    document.getElementById("LEAVE22").textContent = leaveCount + 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
